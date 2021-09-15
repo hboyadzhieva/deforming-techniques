cd4c003a65b8ed2c935b6250e24e19cf9aa8eec4
Hristina Boyadzhieva <hristinaboyadzhieva@gmail.com>
2021-09-15 11:33:24 +0300

    Add comments to file_reader

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,22 @@
+"""Util module to read files from the file system"""
 import numpy as np
 
 
 def read_obj(filename):
-    # FIXME comment properly and see how to give credit to source
-    """https://yzhong-cs.medium.com/beyond-data-scientist-3d-plots-in-python-with-examples-2a8bd7aa654b"""
+    """
+    Reads .obj file from file system and returns vertices and faces data.
+
+    :param filename: absolute path to .obj file
+    :return: array of vertices of the object (numpy.array),
+             array of faces (triangles) of the object (numpy.array)
+
+    *********************************************************************************************************************
+    Title: Beyond data scientist: 3d plots in Python with examples
+    Author: Yuchen Zhong
+    Date: Jul 18, 2019
+    Availability: https://yzhong-cs.medium.com/beyond-data-scientist-3d-plots-in-python-with-examples-2a8bd7aa654b
+    *********************************************************************************************************************
+    """
     triangles = []
     vertices = []
     with open(filename) as file:
